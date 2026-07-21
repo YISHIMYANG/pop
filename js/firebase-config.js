0af26bc2a1c26115ce14254ae51048681d2d9e90
YISHIMYANG <twheart@hanmail.net>
2026-07-21 20:37:31 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,9 @@
-const firebaseConfig = {
-  apiKey: "Firebase 화면에 나온 apiKey",
+window.HANUL_FIREBASE_CONFIG = {
+  apiKey: "Firebase에서 복사한 실제 apiKey",
   authDomain: "hanul-delivery-b62fd.firebaseapp.com",
   databaseURL: "https://hanul-delivery-b62fd-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "hanul-delivery-b62fd",
   storageBucket: "hanul-delivery-b62fd.firebasestorage.app",
   messagingSenderId: "887011669365",
-  appId: "Firebase 화면에 나온 appId"
+  appId: "Firebase에서 복사한 실제 appId"
 };
