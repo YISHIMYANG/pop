c71464e948344ab700bf62bfa7ec59b2c13e2143
YISHIMYANG <twheart@hanmail.net>
2026-07-21 20:29:54 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,9 @@
-/*
-  Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정 및 구성에서
-  아래 값을 복사해 넣으세요. 이 파일은 GitHub에 함께 업로드합니다.
-*/
-window.HANUL_FIREBASE_CONFIG = {
-  apiKey: "Firebase 화면의 apiKey",
+const firebaseConfig = {
+  apiKey: "Firebase 화면에 나온 apiKey",
   authDomain: "hanul-delivery-b62fd.firebaseapp.com",
   databaseURL: "https://hanul-delivery-b62fd-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "hanul-delivery-b62fd",
   storageBucket: "hanul-delivery-b62fd.firebasestorage.app",
-  messagingSenderId: "Firebase 화면의 숫자",
-  appId: "Firebase 화면의 appId"
+  messagingSenderId: "887011669365",
+  appId: "Firebase 화면에 나온 appId"
 };
