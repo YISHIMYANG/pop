7f482bbc2c5ba39735820c400a288acf0f71025f
YISHIMYANG <twheart@hanmail.net>
2026-07-22 00:15:03 +0900

    Update Firebase configuration for new project

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,10 @@
 window.HANUL_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDv8SgpLEfMWojCrFvA0iGnF-5TZW0BQMU",
-  authDomain: "hanul-delivery-b62fd.firebaseapp.com",
-  databaseURL: "https://hanul-delivery-b62fd-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "hanul-delivery-b62fd",
-  storageBucket: "hanul-delivery-b62fd.firebasestorage.app",
-  messagingSenderId: "887011669365",
-  appId: "1:887011669365:web:04116a16e6fdcdb9398f3c"
+ apiKey: "AIzaSyDxxxxxxxxxxxxxxxx",
+  authDomain: "hanul-career-mart.firebaseapp.com",
+  databaseURL:
+    "https://hanul-career-mart-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hanul-career-mart",
+  storageBucket: "hanul-career-mart.firebasestorage.app",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef123456"
 };
