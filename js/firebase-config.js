38d4ac4db0231b3c70da2bcdd8ba077289445985
YISHIMYANG <twheart@hanmail.net>
2026-07-22 00:44:23 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,10 @@
 window.HANUL_FIREBASE_CONFIG = {
  apiKey: "AIzaSyDxxxxxxxxxxxxxxxx",
-  authDomain: "hanul-career-mart.firebaseapp.com",
+  authDomain: "hanul-delivery.firebaseapp.com",
   databaseURL:
     "https://hanul-career-mart-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "hanul-career-mart",
-  storageBucket: "hanul-career-mart.firebasestorage.app",
+  storageBucket: "hanul-delivery.firebasestorage.app",
   messagingSenderId: "123456789012",
   appId: "1:123456789012:web:abcdef123456"
 };
