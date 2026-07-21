0d4800f440d2e1fcd0a738ac2b4226dc65cd048a
YISHIMYANG <twheart@hanmail.net>
2026-07-21 20:48:02 +0900

    Update Firebase configuration with real credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,9 @@
 window.HANUL_FIREBASE_CONFIG = {
-  apiKey: "Firebase에서 복사한 실제 apiKey",
+  apiKey: "AIzaSyDv8SgpLEfMWojCrFvA0iGnF-5TZW0BQMU",
   authDomain: "hanul-delivery-b62fd.firebaseapp.com",
   databaseURL: "https://hanul-delivery-b62fd-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "hanul-delivery-b62fd",
   storageBucket: "hanul-delivery-b62fd.firebasestorage.app",
   messagingSenderId: "887011669365",
-  appId: "Firebase에서 복사한 실제 appId"
+  appId: "1:887011669365:web:04116a16e6fdcdb9398f3c"
 };
