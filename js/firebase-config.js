d68a75ce44d14af5ad03244f35b1c528d11743a8
YISHIMYANG <twheart@hanmail.net>
2026-07-22 00:45:03 +0900

    Update Firebase configuration for delivery project

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,8 +2,8 @@ window.HANUL_FIREBASE_CONFIG = {
  apiKey: "AIzaSyDxxxxxxxxxxxxxxxx",
   authDomain: "hanul-delivery.firebaseapp.com",
   databaseURL:
-    "https://hanul-career-mart-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "hanul-career-mart",
+    "https://hanul-delivery-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hanul-delivery",
   storageBucket: "hanul-delivery.firebasestorage.app",
   messagingSenderId: "123456789012",
   appId: "1:123456789012:web:abcdef123456"
